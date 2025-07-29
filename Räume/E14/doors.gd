1e2c63b6b09f4c98dc638194d2eff69d5f86472b
la fourier <jojoi7@gmx.de>
2025-07-29 14:21:19 +0200

    Proof of concept - door position

--- Räume/E14/doors.gd
+++ Räume/E14/doors.gd
@@ -7,5 +7,9 @@ extends Node2D
 func use_door(door : Area2D):
 	if door == door_left:
 		SceneManager.transition_to_scene("116")
+		Noah.position.x = 40
+		Noah.position.y = 16
 	if door == door_right:
 		SceneManager.transition_to_scene("A01")
+		Noah.position.x = 150
+		Noah.position.y = 16
